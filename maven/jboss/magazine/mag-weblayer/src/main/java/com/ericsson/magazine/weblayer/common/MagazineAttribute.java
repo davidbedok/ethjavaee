3975bde7b4435c65ac6292623338305382c491af
edvibed <david.bedok@ericsson.com>
2017-05-17 21:08:17 +0200

    ADD: magazine project maven, jboss

---
+++ maven/jboss/magazine/mag-weblayer/src/main/java/com/ericsson/magazine/weblayer/common/MagazineAttribute.java
@@ -0,0 +1,8 @@
+package com.ericsson.magazine.weblayer.common;
+
+public interface MagazineAttribute {
+
+	public static final String ATTR_ISNEW = "isnew";
+	public static final String ATTR_MAGAZINE = "magazine";
+
+}
